a78083422360f93cc7186cddcec4d7a208e841d8
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-11 12:46:17 -0400

    adding iam for opensearch provider

--- terraform/modules/iam_role/variables.tf
+++ terraform/modules/iam_role/variables.tf
@@ -21,6 +21,12 @@ variable "iam_assume_role_policy" {
       },
       "Effect": "Allow"
     }
+    {
+      Effect": "Allow",
+      "Principal": {
+        "Service": "opensearchservice.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"}
   ]
 }
 EOF
