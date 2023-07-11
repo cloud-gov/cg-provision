17bff2bad51ae7c5b9e02404ac737412a2c67111
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-11 09:37:58 -0400

    testing 2 templates

--- terraform/modules/opensearch_provider/provider.tf
+++ terraform/modules/opensearch_provider/provider.tf
@@ -2,3 +2,52 @@
 provider "opensearch" {
   url = var.domain_name
 }
+
+resource "opensearch_index" "test" {
+  name               = "terraform-test"
+  number_of_shards   = 1
+  number_of_replicas = 1
+  mappings           = <<EOF
+{
+  "people": {
+    "_all": {
+      "enabled": false
+    },
+    "properties": {
+      "email": {
+        "type": "text"
+      }
+    }
+  }
+}
+EOF
+}
+
+resource "opensearch_component_template" "test" {
+  name = "terraform-test"
+  body = <<EOF
+{
+  "template": {
+    "settings": {
+      "index": {
+        "number_of_shards": 1
+      }
+    },
+    "mappings": {
+      "properties": {
+        "host_name": {
+          "type": "keyword"
+        },
+        "created_at": {
+          "type": "date",
+          "format": "EEE MMM dd HH:mm:ss Z yyyy"
+        }
+      }
+    },
+    "aliases": {
+      "mydata": { }
+    }
+  }
+}
+EOF
+}
