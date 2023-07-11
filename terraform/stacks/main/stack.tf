3aa356e45cf2dce94dfbca081f5cd0b0dff2ad7f
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-11 17:36:22 -0400

    giving more role

--- terraform/stacks/main/stack.tf
+++ terraform/stacks/main/stack.tf
@@ -52,7 +52,7 @@ provider "aws" {
 # Configure the Opensearch provider
 provider "opensearch" {
   url                 = var.opensearch_logs_customer_endpoint
-  aws_assume_role_arn = var.assume_arn
+  #aws_assume_role_arn = var.assume_arn
   # username      = var.opensearch_logs_customer_master_username
   # password      = var.opensearch_logs_customer_master_password
   sign_aws_requests = true
