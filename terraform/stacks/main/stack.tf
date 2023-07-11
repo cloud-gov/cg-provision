3cfa2a3dba16c6365b4726c8191638c4ead51653
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-11 11:04:10 -0400

    moving provider to opensearch domain for easier configuration

--- terraform/stacks/main/stack.tf
+++ terraform/stacks/main/stack.tf
@@ -314,11 +314,6 @@ module "opensearch_logs_customer" {
   master_user_password = var.opensearch_logs_customer_master_password
 }
 
-module "opensearch_provider" {
-  source               = "../../modules/opensearch_provider"
-  domain_name          = "${var.stack_description}-logs-customer"
-}
-
 module "logsearch" {
   source = "../../modules/logsearch"
 
