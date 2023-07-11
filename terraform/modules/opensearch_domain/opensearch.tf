7f1274b1ace65e7d78c25b2dd5918bdcebaaee48
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-11 14:18:38 -0400

    don't neet these

--- terraform/modules/opensearch_domain/opensearch.tf
+++ terraform/modules/opensearch_domain/opensearch.tf
@@ -7,8 +7,8 @@ resource "aws_opensearch_domain" "opensearch" {
     internal_user_database_enabled = var.internal_user_database_enabled
     master_user_options {
       master_user_name     = var.master_user_name
-      master_user_password = var.master_user_password
-      master_user_arn      = var.master_user_arn
+      #master_user_password = var.master_user_password
+      #master_user_arn      = var.master_user_arn
     }
   }
 
