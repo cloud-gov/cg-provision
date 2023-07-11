7ec86dce69573bd66c243ca5e656be72884c2dc2
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-11 12:07:58 -0400

    let other chasnges go through

--- terraform/modules/opensearch_provider/provider.tf
+++ terraform/modules/opensearch_provider/provider.tf
@@ -1,48 +1,48 @@
-resource "opensearch_index" "test" {
-  name               = "terraform-test"
-  number_of_shards   = 1
-  number_of_replicas = 1
-  mappings           = <<EOF
-{
-  "people": {
-    "_all": {
-      "enabled": false
-    },
-    "properties": {
-      "email": {
-        "type": "text"
-      }
-    }
-  }
-}
-EOF
-}
+# resource "opensearch_index" "test" {
+#   name               = "terraform-test"
+#   number_of_shards   = 1
+#   number_of_replicas = 1
+#   mappings           = <<EOF
+# {
+#   "people": {
+#     "_all": {
+#       "enabled": false
+#     },
+#     "properties": {
+#       "email": {
+#         "type": "text"
+#       }
+#     }
+#   }
+# }
+# EOF
+# }
 
-resource "opensearch_component_template" "test2" {
-  name = "terraform-test2"
-  body = <<EOF
-{
-  "template": {
-    "settings": {
-      "index": {
-        "number_of_shards": 1
-      }
-    },
-    "mappings": {
-      "properties": {
-        "host_name": {
-          "type": "keyword"
-        },
-        "created_at": {
-          "type": "date",
-          "format": "EEE MMM dd HH:mm:ss Z yyyy"
-        }
-      }
-    },
-    "aliases": {
-      "mydata": { }
-    }
-  }
-}
-EOF
-}
+# resource "opensearch_component_template" "test2" {
+#   name = "terraform-test2"
+#   body = <<EOF
+# {
+#   "template": {
+#     "settings": {
+#       "index": {
+#         "number_of_shards": 1
+#       }
+#     },
+#     "mappings": {
+#       "properties": {
+#         "host_name": {
+#           "type": "keyword"
+#         },
+#         "created_at": {
+#           "type": "date",
+#           "format": "EEE MMM dd HH:mm:ss Z yyyy"
+#         }
+#       }
+#     },
+#     "aliases": {
+#       "mydata": { }
+#     }
+#   }
+# }
+# EOF
+# }
