4420aeab4dd47dde744579a3c2136d04893f0bc7
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-12 08:44:54 -0400

    rolling dice

--- terraform/stacks/main/versions.tf
+++ terraform/stacks/main/versions.tf
@@ -7,8 +7,7 @@ terraform {
       version = "< 6.0.0"
     }
     opensearch = {
-      source  = "opensearch-project/opensearch"
-      version = "1.0.0"
+      source  = "https://github.com/serge-r/terraform-provider-opensearch.git"
     }
   }
 }
