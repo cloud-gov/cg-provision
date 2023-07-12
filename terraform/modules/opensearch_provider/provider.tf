16e85802808b22cbbbc380d0341dde1f49df2ea4
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-12 10:31:59 -0400

    no username or password

--- terraform/modules/opensearch_provider/provider.tf
+++ terraform/modules/opensearch_provider/provider.tf
@@ -1,56 +1,56 @@
-resource "opensearch_index_template" "test" {
-  name = "template_1"
-  body = <<EOF
-{
-  "template": "te*",
-  "settings": {
-    "number_of_shards": 1
-  },
-  "mappings": {
-    "type1": {
-      "_source": {
-        "enabled": false
-      },
-      "properties": {
-        "host_name": {
-          "type": "keyword"
-        },
-        "created_at": {
-          "type": "date",
-          "format": "EEE MMM dd HH:mm:ss Z YYYY"
-        }
-      }
-    }
-  }
-}
-EOF
-}
+# resource "opensearch_index_template" "test" {
+#   name = "template_1"
+#   body = <<EOF
+# {
+#   "template": "te*",
+#   "settings": {
+#     "number_of_shards": 1
+#   },
+#   "mappings": {
+#     "type1": {
+#       "_source": {
+#         "enabled": false
+#       },
+#       "properties": {
+#         "host_name": {
+#           "type": "keyword"
+#         },
+#         "created_at": {
+#           "type": "date",
+#           "format": "EEE MMM dd HH:mm:ss Z YYYY"
+#         }
+#       }
+#     }
+#   }
+# }
+# EOF
+# }
 
-resource "opensearch_component_template" "test2" {
-  name = "terraform-test"
-  body = <<EOF
-{
-  "template": {
-    "settings": {
-      "index": {
-        "number_of_shards": 1
-      }
-    },
-    "mappings": {
-      "properties": {
-        "host_name": {
-          "type": "keyword"
-        },
-        "created_at": {
-          "type": "date",
-          "format": "EEE MMM dd HH:mm:ss Z yyyy"
-        }
-      }
-    },
-    "aliases": {
-      "mydata": { }
-    }
-  }
-}
-EOF
-}
+# resource "opensearch_component_template" "test2" {
+#   name = "terraform-test"
+#   body = <<EOF
+# {
+#   "template": {
+#     "settings": {
+#       "index": {
+#         "number_of_shards": 1
+#       }
+#     },
+#     "mappings": {
+#       "properties": {
+#         "host_name": {
+#           "type": "keyword"
+#         },
+#         "created_at": {
+#           "type": "date",
+#           "format": "EEE MMM dd HH:mm:ss Z yyyy"
+#         }
+#       }
+#     },
+#     "aliases": {
+#       "mydata": { }
+#     }
+#   }
+# }
+# EOF
+# }
