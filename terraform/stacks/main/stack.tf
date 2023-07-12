5636c09f25c526ecdd4971b6f4456887f4c091b7
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-11 22:05:25 -0400

    attempting without signing

--- terraform/stacks/main/stack.tf
+++ terraform/stacks/main/stack.tf
@@ -55,7 +55,7 @@ provider "opensearch" {
   #aws_assume_role_arn = var.assume_arn
   # username      = var.opensearch_logs_customer_master_username
   # password      = var.opensearch_logs_customer_master_password
-  sign_aws_requests = true
+  #sign_aws_requests = true
   healthcheck = false
 }
 
