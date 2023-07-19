8aa9c2607dcc87d7adee4ed4aa6b3ee376ce599e
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-19 13:57:07 -0400

    trying template

--- terraform/modules/opensearch_domain/index.tf
+++ terraform/modules/opensearch_domain/index.tf
@@ -4,8 +4,50 @@ resource "opensearch_index_template" "testing" {
 {
   "template" : "index_t*",
   "settings": {
-  "number_of_shards": 1
-   }
+    "codec"                                = "best_compression"
+    "number_of_replicas"                   = 1
+    "number_of_shards"                     = 5
+    "search_slowlog_threshold_query_debug" = "30s"
+    "search_slowlog_threshold_query_info"  = "15s"
+    "search_slowlog_threshold_query_trace" = "10s"
+    "search_slowlog_threshold_query_warn"  = "5s"
+   },
+   "mappings": {
+
+    "dynamic_templates": [
+        {
+            "string_fields": {
+            "match": "*",
+            "match_mapping_type": "string",
+            "mapping": {
+                "type": "keyword",
+                "index": true,
+                "norms": false
+            }
+            }
+        }
+        ],
+        "properties": {
+            "@version": {
+                "type": "keyword",
+                "index": true
+                },
+            "@raw": {
+                "type": "text",
+                "index": true,
+                "norms": false
+                },
+            "geoip": {
+                "type": "object",
+                "dynamic": true,
+                "properties": {
+                    "location": {
+                        "type": "geo_point"
+                        }
+                    }
+                }
+            }
+    }
 }
 EOF
 }
@@ -54,48 +96,6 @@ resource "opensearch_index" "index" {
                 }
             }
         }
-}
-EOF
-}
-
-resource "opensearch_component_template" "test" {
-  name = "terraform-test"
-  body = <<EOF
-{
-  "template": {
-    "settings": {
-      "index": {
-        "number_of_shards": 1
-      }
-    },
-    "mappings": {
-      "properties": {
-        "host_name": {
-          "type": "keyword"
-        },
-        "created_at": {
-          "type": "date",
-          "format": "EEE MMM dd HH:mm:ss Z yyyy"
-        }
-      }
-    },
-    "aliases": {
-      "mydata": { }
-    }
-  }
-}
-EOF
-}
-
-resource "opensearch_composable_index_template" "template_1" {
-  name = "template_1"
-  body = <<EOF
-{
-    "index_patterns": ["testing*"],
-    "composed_of": [
-        "terraform-test"
-    ],
-    "priority": 202
 }
 EOF
 }
