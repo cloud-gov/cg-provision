60c930b7c9240e27d9f7f7cb433f04ccb2ab41cf
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-19 09:46:48 -0400

    testing a index filethat is very bare minimum

--- terraform/modules/opensearch_domain/index.tf
+++ terraform/modules/opensearch_domain/index.tf
@@ -2,10 +2,19 @@ resource "opensearch_index_template" "index_template" {
   name     = "test_index"
   body     = <<EOF
 {
-  "template" : {
-    "settings": {
-      "plugins.index_state_management.rollover_alias": "test"
-   }
+  "template" : "test2",
+  "settings": {
+  "number_of_shards": 1
+   },
+   "mappings": {
+    "type1": {
+      "properties": {
+        "created_at": {
+          "type": "date",
+          "format": "EEE MMM dd HH:mm:ss Z YYYY"
+        }
+      }
+    }
   }
 }
 EOF
