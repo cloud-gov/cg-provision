5fb3ce3a5106ddbf1d7ec9d41ac2772de61a0819
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-19 09:31:11 -0400

    test

--- terraform/modules/opensearch_domain/index.tf
+++ terraform/modules/opensearch_domain/index.tf
@@ -1,11 +1,10 @@
 resource "opensearch_index_template" "index_template" {
-  name     = test_index
+  name     = "test_index"
   body     = <<EOF
 {
-  "index_patterns": ["${each.key}*"],
   "template" : {
     "settings": {
-      "plugins.index_state_management.rollover_alias": "${each.key}"
+      "plugins.index_state_management.rollover_alias": "test"
    }
   }
 }
