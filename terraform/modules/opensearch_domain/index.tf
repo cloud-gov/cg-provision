e5db827e7b5c9f4818cadc133b88602e5747705c
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-19 14:11:31 -0400

    trying template

--- terraform/modules/opensearch_domain/index.tf
+++ terraform/modules/opensearch_domain/index.tf
@@ -7,10 +7,18 @@ resource "opensearch_index_template" "testing" {
 		"codec": "best_compression",
 		"number_of_replicas": 1,
 		"number_of_shards": 5,
-		"search_slowlog_threshold_query_debug": "30s",
-		"search_slowlog_threshold_query_info": "15s",
-		"search_slowlog_threshold_query_trace": "10s",
-		"search_slowlog_threshold_query_warn": "5s"
+		"search": {
+			"slowlog": {
+				"threshold": {
+					"query": {
+						"warn": "30s",
+						"info": "15s",
+						"debug": "10s",
+						"trace": "5s"
+					}
+				}
+			}
+		}
 	},
 	"mappings": {
 
