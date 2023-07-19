f76e2b8b29b4f2e4aee70a3b59efe6fe1a9cb09a
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-19 13:26:17 -0400

    testing composable

--- terraform/modules/opensearch_domain/index.tf
+++ terraform/modules/opensearch_domain/index.tf
@@ -1,5 +1,5 @@
-resource "opensearch_index_template" "index_template" {
-  name     = "index_template"
+resource "opensearch_index_template" "logs-app-*" {
+  name     = testing
   body     = <<EOF
 {
   "template" : "index_t*",
@@ -54,6 +54,48 @@ resource "opensearch_index" "index" {
                 }
             }
         }
+}
+EOF
+}
+
+resource "opensearch_component_template" "test" {
+  name = "terraform-test"
+  body = <<EOF
+{
+  "template": {
+    "settings": {
+      "index": {
+        "number_of_shards": 1
+      }
+    },
+    "mappings": {
+      "properties": {
+        "host_name": {
+          "type": "keyword"
+        },
+        "created_at": {
+          "type": "date",
+          "format": "EEE MMM dd HH:mm:ss Z yyyy"
+        }
+      }
+    },
+    "aliases": {
+      "mydata": { }
+    }
+  }
+}
+EOF
+}
+
+resource "opensearch_composable_index_template" "template_1" {
+  name = "template_1"
+  body = <<EOF
+{
+    "index_patterns": ["testing*"],
+    "composed_of": [
+        "terraform-test"
+    ],
+    "priority": 202
 }
 EOF
 }
