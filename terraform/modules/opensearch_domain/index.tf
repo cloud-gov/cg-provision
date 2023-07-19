b5c8d392d4b23c8006e436b58f738c64494987b1
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-19 11:17:17 -0400

    index-settings added

--- terraform/modules/opensearch_domain/index.tf
+++ terraform/modules/opensearch_domain/index.tf
@@ -11,11 +11,17 @@ EOF
 }
 
 resource "opensearch_index" "index" {
-  name     = "index"
-  number_of_shards = 1
-  number_of_replicas = 1
+  name                                 = "index"
+  codec                                = "best_compression"
+  number_of_shards                     = 1
+  number_of_replicas                   = 1
+  search_slowlog_threshold_query_debug = "30s"
+  search_slowlog_threshold_query_info  = "15s"
+  search_slowlog_threshold_query_trace = "10s"
+  search_slowlog_threshold_query_warn  = "5s"
   mappings     = <<EOF
 {
 }
 EOF
 }
+
